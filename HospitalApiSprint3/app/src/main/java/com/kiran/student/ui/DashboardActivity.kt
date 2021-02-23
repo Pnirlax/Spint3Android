8ffe42cf2988845332b511624a5e1479aa93cf9c
“puja123” <pnirlax@gmail.com>
2021-02-23 20:56:29 +0545

    hms

--- HospitalApiSprint3/app/src/main/java/com/kiran/student/ui/DashboardActivity.kt
+++ HospitalApiSprint3/app/src/main/java/com/kiran/student/ui/DashboardActivity.kt
@@ -11,17 +11,22 @@ import com.kiran.student.api.ServiceBuilder
 class DashboardActivity : AppCompatActivity() {
     private lateinit var btnAddStudent : Button
     private lateinit var btnDisplayStudent : Button
+    private lateinit var btnLocation : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         Toast.makeText(this, ServiceBuilder.token.toString(), Toast.LENGTH_SHORT).show()
         btnAddStudent = findViewById(R.id.btnAddStudent)
         btnDisplayStudent = findViewById(R.id.btnDisplayStudent)
+       // btnLocation = findViewById(R.id.btnLocation)
         btnAddStudent.setOnClickListener{
             startActivity(Intent(this, AddStudentActivity::class.java))
         }
         btnDisplayStudent.setOnClickListener{
             startActivity(Intent(this, DisplayStudentActivity::class.java))
         }
+//        btnLocation.setOnClickListener {
+//
+//        }
     }
 }
